53f3570daf32bd1974202c6c5abfc15c8e265567
gin <73906888+gingkapls@users.noreply.github.com>
2025-11-03 07:09:33 +0000

    fix: add urlencoded middleware, signup router

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import "dotenv/config";
+import express, { urlencoded } from "express";
 import { indexRouter } from "./routes/index.js";
 import { resolve } from "node:path";
 import { signUpRouter } from "./routes/signUpRouter.js";
@@ -10,9 +11,11 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 app.set("views", resolve(import.meta.dirname, "views"));
 
+app.use(urlencoded({ extended: false }));
+
 // routes
 app.use("/", indexRouter);
-app.use('/sign-up', signUpRouter)
+app.use("/sign-up", signUpRouter);
 
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
